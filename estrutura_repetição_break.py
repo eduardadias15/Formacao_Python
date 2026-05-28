b693728b63ee00203a38381c4be35d1b2caa0ba2
Eduarda Dias <eduardagomesdias76@gmail.com>
2026-05-28 16:10:24 -0300

    continuação das estruturas

---
+++ estrutura_repetição_break.py
@@ -0,0 +1,9 @@
+opção = -1
+
+while True:
+    número = input(" Informe um número:")
+
+    if número == "0":
+     break 
+
+    print(número)
---
+++ estrutura_repetição_while.py
@@ -0,0 +1,13 @@
+opção = -1
+
+while opção !=0:
+    opção = input(" [1] Sacar \n [2] Extrato \n [0] Sair \n Digite a opção desejada:")
+
+    if opção == "1":
+        print("Sacando...\n")
+
+    elif opção == "2":
+        print("Exibindo extrato...\n")
+
+    else: 
+        print("Obrigado por usar o nosso sitema bancario, volte sempre!\n")
