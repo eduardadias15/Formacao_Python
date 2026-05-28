3383dda080624a1881912020b58a234a18879714
Eduarda Dias <eduardagomesdias76@gmail.com>
2026-05-28 16:23:21 -0300

    Update estrutura_repetição_break.py

--- estrutura_repetição_break.py
+++ estrutura_repetição_break.py
@@ -4,6 +4,14 @@ while True:
     número = input(" Informe um número:")
 
     if número == "0":
-     break 
+        break 
 
     print(número)
+
+
+
+for número in range (100):
+   
+   if número ==10:
+         break
+   print (número, end = " ")
