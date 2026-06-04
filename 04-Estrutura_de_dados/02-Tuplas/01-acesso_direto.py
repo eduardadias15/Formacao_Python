2416e21615ccf0b7e450f767dca5ef2acae4c68f
Eduarda Dias <eduardagomesdias76@gmail.com>
2026-06-03 21:48:29 -0300

    Tuplas

---
+++ 04-Estrutura_de_dados/02-Tuplas/01-acesso_direto.py
@@ -0,0 +1,19 @@
+# Tuplas são estruturas de dados imutáveis, ou seja, não podem ser alteradas após a sua criação.
+# Elas são definidas usando parênteses () e os elementos são separados por vírgulas.
+# As tuplas podem conter elementos de diferentes tipos, como strings, números e outras tuplas.
+
+frutas = (
+    "laranja",
+    "pera",
+    "uva",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
---
+++ 04-Estrutura_de_dados/02-Tuplas/02_negativos.py
@@ -0,0 +1,9 @@
+frutas = (
+    "maçã",
+    "morango",
+    "uva",
+    "limão",
+)
+
+print(frutas[-1])  # limão
+print(frutas[-3])  # morango
