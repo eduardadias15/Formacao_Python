8049ccc46ec798a0a111206f933fbf8e1ee6741f
Eduarda Dias <eduardagomesdias76@gmail.com>
2026-06-30 11:51:15 -0300

    exercícios

---
+++ Exercicios/conversao_realpdollar.py
@@ -0,0 +1,3 @@
+X=float(input('Quanto reais você tem na carteira? R$'))
+D= X/5.19
+print('Com R${} você tem us${:.2f} dolares'.format(X,D))
---
+++ Exercicios/tabuada.py
@@ -0,0 +1,2 @@
+X=int(input('Digite um número: '))
+print('A tabuada de {} é:\n {} x 1 = {}\n {} x 2 = {}\n {} x 3 = {}\n {} x 4 = {}\n {} x 5= {}\n {} x 6 = {}\n {} x 7 = {}\n {} x 8 = {}\n {} x 9 = {}\n {} x 10 = {}'.format (X, X, X*1, X, X*2, X, X*3, X, X*4, X, X*5, X, X*6, X, X*7, X, X*8, X, X*9, X, X*10))
